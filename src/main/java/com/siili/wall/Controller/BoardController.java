100fd3ae390e457677449bbcc7e6dac723f9a88f
valtteri <valtteri.koivunen@hotmail.com>
2020-03-06 09:34:20 +0000

    Crossorigin

--- src/main/java/com/siili/wall/Controller/BoardController.java
+++ src/main/java/com/siili/wall/Controller/BoardController.java
@@ -6,6 +6,7 @@ import com.siili.wall.Domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,6 +49,7 @@ public class BoardController {
 
     //This operator saves a column into a board
     @RequestMapping(value="/boards/{id}/columns", method=RequestMethod.GET)
+    @CrossOrigin
     public String boardsAddColumn(@PathVariable("id") Long boardId, @PathVariable("id") Long columnId, Model model, Column column) {
         Optional<Board> board = brepository.findById(boardId);
         if (!board.get().hasColumn(column)) {
@@ -60,6 +62,7 @@ public class BoardController {
 
     //This operator saves a card into a column
     @RequestMapping(value="/columns/{id}/cards", method=RequestMethod.GET)
+    @CrossOrigin
     public String columnsAddCard(@PathVariable("id") Long columnId, @PathVariable("id") Long cardId, Model model, Card card) {
         Optional<Column> column = crepository.findById(columnId);
         if (!column.get().hasCard(card)) {
