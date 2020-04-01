ce198a03a5bb2f94a884c59e9be252e900abbd05
valtteri <valtteri.koivunen@hotmail.com>
2020-04-01 10:20:43 +0100

    perma database

--- src/main/java/com/siili/wall/Domain/ColumnRepository.java
+++ src/main/java/com/siili/wall/Domain/ColumnRepository.java
@@ -1,10 +1,7 @@
 package com.siili.wall.Domain;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface ColumnRepository extends CrudRepository <Column, Long> {
-    List<Column> findByColumn(String column);
+
 }
